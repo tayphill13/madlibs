530c633b65ce659e3635360471024a12bfbf488f
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-03 13:20:43 -0700

    add event.preventDefault() to scripts.js
    Co-authored-by: Taylor Phillips<taylorphillips133@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    event.preventDefault()
+    
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput= $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+    console.log(animalInput);
+
+    $("person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+    
+    $("#story").show();
+  });
+});
