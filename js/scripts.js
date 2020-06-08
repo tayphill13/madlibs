698ccb191df42c37a9df0d063d34d9bec662fe17
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-08 14:03:01 -0700

    refactor the scripts.js to use a forEach loop to gather values in an array and then to push them to the html
    Co-Authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
-    event.preventDefault()
+    event.preventDefault();
     
-    const person1Input = $("input#person1").val();
+    /*const person1Input = $("input#person1").val();   'Building without array'
     const person2Input = $("input#person2").val();
     const animalInput= $("input#animal").val();
     const exclamationInput = $("input#exclamation").val();
     const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+    const nounInput = $("input#noun").val();*/
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
+    //const inputArray = [];
+    //inputArray.push($("input#person1").val(), $("input#person2").val(), $("input#animal").val(), $("input#exclamation").val(), $("input#verb").val(), $("input#noun").val());  
+
+    const spanClasses = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+    spanClasses.forEach(function(spanClass){
+      let input = $("input#"+spanClass).val(); // let person2Input = $("input#person2").val();
+      $("."+spanClass).text(input); //$(".person2").text(person2Input);
+    });
+    /*$(".person1").text(inputArray[0]);
+    $(".person2").text(inputArray[1]);
+    $(".animal").text(inputArray[2]);
     $(".exclamation").text(exclamationInput);
     $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    $(".noun").text(nounInput);*/
     
     $("#story").show();
   });
